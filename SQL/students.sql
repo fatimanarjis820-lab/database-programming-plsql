faa0ce3ebece738763776563f8f315cd9983e536
Lab User <fatimanarjis820@gmail.com>
2026-02-01 23:47:26 +0530

    Add SQL file and README.md

---
+++ SQL/students.sql
@@ -0,0 +1,13 @@
+CREATE TABLE students 
+(
+    student_id NUMBER, 
+    student_name VARCHAR2(4)
+
+);
+
+
+INSERT INTO student (student_id, student_name) VALUES (101, 'Johnn');
+
+SELECT * FROM student;
+
+
